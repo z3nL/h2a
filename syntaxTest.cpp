f248497dfe19cc9c0f801b5c8b33e6d2fe065ab0
Zen L <zenlambertus@gmail.com>
2024-09-27 19:21:36 -0400

    hello blake and seba
    blah

---
+++ syntaxTest.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+class testClass {
+    private:
+        int val;
+        string name;
+}
+
+int main(void) {
+    printf("hello friend");
+}
