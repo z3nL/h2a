f9b29e52da26d67f19e07f35ec7078b6cd81f9f6
BlakeCoppens <Blakecoppensbusiness@gmail.com>
2024-09-28 15:46:49 -0400

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,13 +2,40 @@ from flask import Flask,render_template, request
 from flask_mysqldb import MySQL
  
 app = Flask(__name__)
- 
+
+
+
+
+def loginpg():
+    return render_template('login.html')
+def loggedinpg():
+    return render_template('loggedin.html')
+def settingspg():
+    return render_template('settings.html')
+def transactionspg():
+    return render_template('transactions.html')
+
+# Get the variables for the the the the login
+@app.route('/login',methods=['GET', 'POST'])
+def signIn():
+    username = request.form['username']
+    password = request.form['password']
+    print(f"Username: {username}, Password: {password}")
+    
+    
+    # At this point, check if the username is equal to something in SQL 
+    # base and then verify that the password is equal, otherwise cause 
+    # error message
+    
+    
+    
+
+
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flask'
- 
-mysql = MySQL(app)
 
 mysql = MySQL(app)
  
