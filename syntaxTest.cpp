5e6f790d6178b5ea6a1da11882fbe5810050ebbc
Zen L <zenlambertus@gmail.com>
2024-09-28 10:34:29 -0400

    blah

--- syntaxTest.cpp
+++ syntaxTest.cpp
@@ -1,12 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-class testClass {
-    private:
-        int val;
-        string name;
-}
+#include <iostream>
+#include "testClass.h"
+using namespace std;
 
 int main(void) {
-    printf("hello friend");
+    testClass obj;
+    
+    obj.changeVal(17);
+    obj.changeVal2(38);
+    obj.changeName("ay");
+
+    cout << obj.getVal() << " " << obj.getVal2() << " " << obj.getName();
+
+    return 0;
 }
